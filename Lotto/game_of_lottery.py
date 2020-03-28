4e8893be804b68dbf424c36d8d910ac15b473c65
Kinjal <58638514+Koraavo@users.noreply.github.com>
2020-03-28 23:48:09 +0200

    Update game_of_lottery.py

--- Lotto/game_of_lottery.py
+++ Lotto/game_of_lottery.py
@@ -3,34 +3,54 @@ import random
 lottery_nums = list(range(1, 91))
 
 ticket_1 = random.sample(lottery_nums, k=15)
-print(f"The winning ticket is: \n{ticket_1[0:5]}\n{ticket_1[5:10]}\n{ticket_1[10:15]}")
+print(f"Ticket_1 is: \n{ticket_1[0:5]}\n{ticket_1[5:10]}\n{ticket_1[10:15]}")
 
 print("")
 
 ticket_2 = random.sample(lottery_nums, k=15)
-print(f"The winning ticket is: \n{ticket_2[0:5]}\n{ticket_2[5:10]}\n{ticket_2[10:15]}")
+print(f"Ticket_2 is: \n{ticket_2[0:5]}\n{ticket_2[5:10]}\n{ticket_2[10:15]}")
 
 print("")
 
 # randomly choosing a number from the list and removing the number from the main list
 count = 0
+pass1 = True
+pass2 = True
+
+
 while True:
     random.shuffle(lottery_nums)
     pop1 = lottery_nums.pop(0)
     print(" The number called is", pop1)
     count += 1
+
     if pop1 in ticket_1:
         match = ticket_1.pop(ticket_1.index(pop1))
-        print("Yayyy")
-    else:
-        print("Bad Luck")
+        print(f"Ticket1 has {match}")
+
+    if pop1 in ticket_2:
+        match = ticket_2.pop(ticket_2.index(pop1))
+        print(f"Ticket2 has {match}")
+
     if not ticket_1:
+        pass1 = False
         break
+
+    if not ticket_2:
+        pass2 = False
+        break
+
     if not lottery_nums:
         break
 
-print(f"All the numbers in ticket1 were found in {count} counts")
-#print(f"All the numbers in the {ticket_1} were found in {count} counts")
+if not pass1:
+    print(f"All the numbers in ticket1 were found in {count} counts\n")
+    print("Ticket1 has won")
+
+if not pass2:
+    print(f"All the numbers in ticket2 were found in {count} counts\n")
+    print("Ticket2 won")
+
 
 
 # IN SETS THE POSITION DOES NOT MATTER
